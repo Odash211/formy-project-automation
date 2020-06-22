0a8860e8d7fc90a52d3c00547d0ab6aabc49a1ba
Odash211 <62903928+Odash211@users.noreply.github.com>
2020-06-22 00:17:10 -0700

    Initial commt

---
+++ 02_03/start/test/Autocomplete.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/ounoc/Downloads/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.click();
+        autocomplete.sendKeys("2538 Island Drive");
+
+        driver.quit();
+    }
+}
